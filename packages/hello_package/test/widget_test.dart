f94a86232a0c7a11155cca239a131de32c6a1553
fzyzcjy <ch271828n@outlook.com>
2022-09-22 15:46:26 +0800

    impl more, half done

--- packages/hello_package/test/widget_test.dart
+++ packages/hello_package/test/widget_test.dart
@@ -9,43 +9,48 @@ void main() {
   testWidgets('forest', (tester) async {
     debugPrintBeginFrameBanner = debugPrintEndFrameBanner = true;
 
-    final secondTree = SecondTree();
+    final secondTreePack = SecondTreePack();
 
     print('before pumpWidget');
-    secondTree.rootView.scheduleInitialLayout();
+    secondTreePack.rootView.scheduleInitialLayout();
 
+    var mainTreeBuilderBuildCount = 0;
+    late StateSetter mainTreeStatefulBuilderSetState;
     await tester.pumpWidget(MaterialApp(
       home: Scaffold(
         body: StatefulBuilder(builder: (_, setState) {
-          SchedulerBinding.instance.addPostFrameCallback((_) {
-            print('mainTree(StatefulBuilder).setState');
-            setState(() {});
-          });
+          mainTreeStatefulBuilderSetState = setState;
+          mainTreeBuilderBuildCount++;
+          print(
+              'main tree StatefulBuilder.builder callback ($mainTreeBuilderBuildCount)');
 
-          for (var iter = 0; iter < 3; ++iter) {
-            print(
-                'mainTree(StatefulBuilder).builder, run second tree pipeline iter=#$iter');
+          // for (var iter = 0; iter < 3; ++iter) {
+          //   print(
+          //       'mainTree(StatefulBuilder).builder, run second tree pipeline iter=#$iter');
+          //
+          //   secondTreePack.innerStatefulBuilderSetState(() {});
+          //
+          //   // NOTE reference: WidgetsBinding.drawFrame & RendererBinding.drawFrame
+          //   // https://github.com/fzyzcjy/yplusplus/issues/5778#issuecomment-1254490708
+          //   secondTreePack.buildOwner.buildScope(secondTreePack.element);
+          //   secondTreePack.pipelineOwner.flushLayout();
+          //   secondTreePack.pipelineOwner.flushCompositingBits();
+          //   secondTreePack.pipelineOwner.flushPaint();
+          //   // renderView.compositeFrame(); // this sends the bits to the GPU
+          //   // pipelineOwner.flushSemantics(); // this also sends the semantics to the OS.
+          //   secondTreePack.buildOwner.finalizeTree();
+          //   print('rootView.size=${secondTreePack.rootView.size}');
+          // }
 
-            secondTree.innerStatefulBuilderSetState(() {});
-
-            // NOTE reference: WidgetsBinding.drawFrame & RendererBinding.drawFrame
-            // https://github.com/fzyzcjy/yplusplus/issues/5778#issuecomment-1254490708
-            secondTree.buildOwner.buildScope(secondTree.element);
-            secondTree.pipelineOwner.flushLayout();
-            secondTree.pipelineOwner.flushCompositingBits();
-            secondTree.pipelineOwner.flushPaint();
-            // renderView.compositeFrame(); // this sends the bits to the GPU
-            // pipelineOwner.flushSemantics(); // this also sends the semantics to the OS.
-            secondTree.buildOwner.finalizeTree();
-            print('rootView.size=${secondTree.rootView.size}');
-          }
-
-          return SizedBox(width: 20, height: 20);
+          return SecondTreeAdapterWidget(
+            child: SizedBox(width: 20, height: 20),
+          );
         }),
       ),
     ));
 
     for (var i = 0; i < 5; ++i) {
+      mainTreeStatefulBuilderSetState(() {});
       await tester.pump();
     }
 
@@ -53,16 +58,39 @@ void main() {
   });
 }
 
-class SecondTree {
+class SecondTreeAdapterWidget extends SingleChildRenderObjectWidget {
+  const SecondTreeAdapterWidget({
+    super.key,
+    super.child,
+  });
+
+  @override
+  RenderSecondTreeAdapter createRenderObject(BuildContext context) =>
+      RenderSecondTreeAdapter();
+
+  @override
+  void updateRenderObject(
+      BuildContext context, RenderSecondTreeAdapter renderObject) {}
+}
+
+class RenderSecondTreeAdapter extends RenderShiftedBox {
+  RenderSecondTreeAdapter({
+    RenderBox? child,
+  }) : super(child);
+
+  // TODO handle layout!
+}
+
+class SecondTreePack {
   late final PipelineOwner pipelineOwner;
   late final MeasurementView rootView;
   late final BuildOwner buildOwner;
   late final RenderObjectToWidgetElement<RenderBox> element;
 
-  var innerStatefulBuilderBuildTime = 0;
+  var innerStatefulBuilderBuildCount = 0;
   late StateSetter innerStatefulBuilderSetState;
 
-  SecondTree() {
+  SecondTreePack() {
     pipelineOwner = PipelineOwner();
     rootView = pipelineOwner.rootNode = MeasurementView();
     buildOwner = BuildOwner(
@@ -73,13 +101,13 @@ class SecondTree {
 
     final secondTreeWidget = StatefulBuilder(builder: (_, setState) {
       print(
-          'secondTreeWidget(StatefulBuilder).builder called ($innerStatefulBuilderBuildTime)');
+          'secondTreeWidget(StatefulBuilder).builder called ($innerStatefulBuilderBuildCount)');
 
       innerStatefulBuilderSetState = setState;
-      innerStatefulBuilderBuildTime++;
+      innerStatefulBuilderBuildCount++;
 
       return SizedBox(
-          width: innerStatefulBuilderBuildTime.toDouble(), height: 10);
+          width: innerStatefulBuilderBuildCount.toDouble(), height: 10);
     });
 
     element = RenderObjectToWidgetAdapter<RenderBox>(
