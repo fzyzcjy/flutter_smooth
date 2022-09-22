a3f32367f4c7c64cef983ef3d50fd33287c7309c
fzyzcjy <ch271828n@outlook.com>
2022-09-22 16:03:51 +0800

    fix bugs

--- packages/hello_package/lib/main.dart
+++ packages/hello_package/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 
 void main() {
+  debugPrintBeginFrameBanner = debugPrintEndFrameBanner = true;
   runApp(const MyApp());
 }
 
@@ -26,8 +27,8 @@ class _MyAppState extends State<MyApp> {
     print('$runtimeType.build ($buildCount)');
 
     if (buildCount < 5) {
-      SchedulerBinding.instance.addPostFrameCallback((_) {
-        print('$runtimeType.setState');
+      Future.delayed(Duration(seconds: 1), () {
+        print('$runtimeType.setState after a second');
         setState(() {});
       });
     }
@@ -42,37 +43,57 @@ class _MyAppState extends State<MyApp> {
   Widget _buildBody() {
     return Column(
       children: [
-        Text('A$buildCount'),
-        Text('B$buildCount'),
-        const MyWidget(),
-        Text('C$buildCount'),
+        Text('A$buildCount', style: TextStyle(fontSize: 30)),
+        Text('B$buildCount', style: TextStyle(fontSize: 30)),
+        MyWidget(parentBuildCount: buildCount),
+        Text('C$buildCount', style: TextStyle(fontSize: 30)),
       ],
     );
   }
 }
 
 class MyWidget extends SingleChildRenderObjectWidget {
+  final int parentBuildCount;
+
   const MyWidget({
     super.key,
+    required this.parentBuildCount,
     super.child,
   });
 
   @override
-  MyRender createRenderObject(BuildContext context) => MyRender();
+  MyRender createRenderObject(BuildContext context) => MyRender(
+        parentBuildCount: parentBuildCount,
+      );
 
   @override
-  void updateRenderObject(BuildContext context, MyRender renderObject) {}
+  void updateRenderObject(BuildContext context, MyRender renderObject) {
+    renderObject.parentBuildCount = parentBuildCount;
+  }
 }
 
 class MyRender extends RenderProxyBox {
   MyRender({
+    required int parentBuildCount,
     RenderBox? child,
-  }) : super(child);
+  })  : _parentBuildCount = parentBuildCount,
+        super(child);
+
+  int get parentBuildCount => _parentBuildCount;
+  int _parentBuildCount;
+
+  set parentBuildCount(int value) {
+    if (_parentBuildCount == value) return;
+    _parentBuildCount = value;
+    print('$runtimeType markNeedsLayout because parentBuildCount changes');
+    markNeedsLayout();
+  }
 
   @override
   void performLayout() {
     // unconditionally call this, as an experiment
     pseudoPreemptRender();
+
     super.performLayout();
   }
 
@@ -82,14 +103,18 @@ class MyRender extends RenderProxyBox {
     // ref: https://github.com/fzyzcjy/yplusplus/issues/5780#issuecomment-1254562485
     final recorder = PictureRecorder();
     final canvas = Canvas(recorder);
-    final rect = Rect.fromLTWH(30, 200, 300, 300);
-    canvas.drawCircle(Offset(10, 20), 10, Paint()..color = Colors.red);
+    final rect = Rect.fromLTWH(0, 0, 300, 300);
+    final paint = Paint()
+      ..color = Colors.primaries[parentBuildCount % Colors.primaries.length];
+    canvas.drawCircle(Offset(150, 150), 100, paint);
     final pictureLayer = PictureLayer(rect);
     pictureLayer.picture = recorder.endRecording();
     final rootLayer = OffsetLayer();
     rootLayer.append(pictureLayer);
     final builder = SceneBuilder();
     final scene = rootLayer.buildScene(builder);
+
+    print('call window.render');
     window.render(scene);
 
     print('$runtimeType pseudoPreemptRender end');
