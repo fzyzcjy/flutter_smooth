d591266f47fc8fbce8d4126c85f69f4981509ae1
fzyzcjy <ch271828n@outlook.com>
2022-09-22 15:53:36 +0800

    impl main.dart expr

--- packages/hello_package/lib/main.dart
+++ packages/hello_package/lib/main.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: avoid_print, prefer_const_constructors
+
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/scheduler.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +18,80 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  var buildCount = 0;
+
   @override
   Widget build(BuildContext context) {
+    buildCount++;
+    print('$runtimeType.build ($buildCount)');
+
+    if (buildCount < 5) {
+      SchedulerBinding.instance.addPostFrameCallback((_) {
+        print('$runtimeType.setState');
+        setState(() {});
+      });
+    }
+
     return MaterialApp(
       home: Scaffold(
-        body: TODO,
+        body: _buildBody(),
       ),
     );
+  }
+
+  Widget _buildBody() {
+    return Column(
+      children: [
+        Text('A$buildCount'),
+        Text('B$buildCount'),
+        const MyWidget(),
+        Text('C$buildCount'),
+      ],
+    );
+  }
+}
+
+class MyWidget extends SingleChildRenderObjectWidget {
+  const MyWidget({
+    super.key,
+    super.child,
+  });
+
+  @override
+  MyRender createRenderObject(BuildContext context) => MyRender();
+
+  @override
+  void updateRenderObject(BuildContext context, MyRender renderObject) {}
+}
+
+class MyRender extends RenderProxyBox {
+  MyRender({
+    RenderBox? child,
+  }) : super(child);
+
+  @override
+  void performLayout() {
+    // unconditionally call this, as an experiment
+    pseudoPreemptRender();
+    super.performLayout();
+  }
+
+  void pseudoPreemptRender() {
+    print('$runtimeType pseudoPreemptRender start');
+
+    // ref: https://github.com/fzyzcjy/yplusplus/issues/5780#issuecomment-1254562485
+    final recorder = PictureRecorder();
+    final canvas = Canvas(recorder);
+    final rect = Rect.fromLTWH(30, 200, 300, 300);
+    canvas.drawCircle(Offset(10, 20), 10, Paint()..color = Colors.red);
+    final pictureLayer = PictureLayer(rect);
+    pictureLayer.picture = recorder.endRecording();
+    final rootLayer = OffsetLayer();
+    rootLayer.append(pictureLayer);
+    final builder = SceneBuilder();
+    final scene = rootLayer.buildScene(builder);
+    window.render(scene);
+
+    print('$runtimeType pseudoPreemptRender end');
   }
 }
