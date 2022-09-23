8fb1db3dae53f74dee3ead24b892ad55eedb2483
fzyzcjy <ch271828n@outlook.com>
2022-09-23 14:42:51 +0800

    let it animate

--- packages/hello_package/lib/main_multi_render.dart
+++ packages/hello_package/lib/main_multi_render.dart
@@ -5,16 +5,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 
+var frameCount = 0;
+
 void beginFrame(Duration timeStamp) {
-  print('onBeginFrame start');
+  frameCount++;
+  print('onBeginFrame start ($frameCount)');
 
   // https://book.flutterchina.club/chapter14/paint.html#_14-5-1-flutter-%E7%BB%98%E5%88%B6%E5%8E%9F%E7%90%86
   final builder = SceneBuilder();
   final recorder = PictureRecorder();
   final canvas = Canvas(recorder);
   final rect = Rect.fromLTWH(0, 0, 500, 500);
-  canvas.drawRect(
-      Rect.fromLTWH(100, 100, 200, 200), Paint()..color = Colors.green);
+  canvas.drawRect(Rect.fromLTWH(100, 100, 200, 200),
+      Paint()..color = Colors.green[(1 + frameCount % 8) * 100]!);
   final pictureLayer = PictureLayer(rect);
   pictureLayer.picture = recorder.endRecording();
   final rootLayer = OffsetLayer();
@@ -25,7 +28,15 @@ void beginFrame(Duration timeStamp) {
   window.render(scene);
   print('call window.render end');
 
-  print('onBeginFrame start');
+  Future.delayed(const Duration(seconds: 1), () {
+    if (frameCount < 5) {
+      print('call scheduleFrame start');
+      PlatformDispatcher.instance.scheduleFrame();
+      print('call scheduleFrame end');
+    }
+  });
+
+  print('onBeginFrame end');
 }
 
 // This function is the primary entry point to your application. The engine
@@ -40,7 +51,7 @@ void main() {
   // Here we kick off the whole process by asking the engine to schedule a new
   // frame. The engine will eventually call onBeginFrame when it is time for us
   // to actually produce the frame.
-  print('call scheduleFrame start');
+  print('main call scheduleFrame start');
   PlatformDispatcher.instance.scheduleFrame();
-  print('call scheduleFrame end');
+  print('main call scheduleFrame end');
 }
