0b86b2005048ea523bf23e01eff0eedb99408d36
fzyzcjy <ch271828n@outlook.com>
2022-09-27 19:10:16 +0800

    remove unused code #11

--- packages/smooth/example/lib/animation.dart
+++ packages/smooth/example/lib/animation.dart
@@ -3,8 +3,6 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:smooth/smooth.dart';
 
 const _kDuration = Duration(milliseconds: 300);
@@ -13,9 +11,7 @@ const _kDuration = Duration(milliseconds: 300);
 
 enum Mode {
   slowByAnimation,
-  slowByBuilder,
   fastByAnimation,
-  fastByBuilder,
 }
 
 class EnterPageAnimation extends StatelessWidget {
@@ -35,12 +31,8 @@ class EnterPageAnimation extends StatelessWidget {
         return const SizedBox();
       case Mode.slowByAnimation:
         return _EnterPageAnimationSlowByAnimation(child: child);
-      case Mode.slowByBuilder:
-        return _EnterPageAnimationSlowByBuilder(child: child);
       case Mode.fastByAnimation:
         return _EnterPageAnimationFastByAnimation(child: child);
-      case Mode.fastByBuilder:
-        return _EnterPageAnimationFastByBuilder(child: child);
     }
   }
 }
@@ -96,52 +88,6 @@ class _EnterPageAnimationSlowByAnimationState
   }
 }
 
-class _EnterPageAnimationSlowByBuilder extends StatefulWidget {
-  final Widget child;
-
-  const _EnterPageAnimationSlowByBuilder({required this.child});
-
-  @override
-  State<_EnterPageAnimationSlowByBuilder> createState() =>
-      _EnterPageAnimationSlowByBuilderState();
-}
-
-class _EnterPageAnimationSlowByBuilderState
-    extends State<_EnterPageAnimationSlowByBuilder> {
-  final counter = Counter();
-  final animation = _SimpleAnimation();
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (_, constraints) {
-      counter.inc();
-      animation.init();
-      final ratio = animation.computeRatio();
-
-      if (ratio < 1) {
-        SchedulerBinding.instance.addPostFrameCallback((_) {
-          // print(
-          //     '$runtimeType.build addPostFrameCallback callback call setState');
-          setState(() {});
-        });
-      }
-
-      return Stack(
-        children: [
-          Positioned(
-            left: constraints.maxWidth * max(0, 1 - ratio),
-            top: 0,
-            bottom: 0,
-            width: constraints.maxWidth,
-            child: widget.child,
-          ),
-          Center(child: counter.build()),
-        ],
-      );
-    });
-  }
-}
-
 class _EnterPageAnimationFastByAnimation extends StatefulWidget {
   final Widget child;
 
@@ -160,44 +106,10 @@ class _EnterPageAnimationFastByAnimationState
     return PreemptBuilder(
       builder: (_, child) =>
           _EnterPageAnimationFastByAnimationInner(child: child),
-      // builder: (_, child) {
-      //   print('$runtimeType.PreemptBuilder.builder callback called');
-      //   // return ColorFiltered(
-      //   //   colorFilter: invertColorFilter,
-      //   //   child: child,
-      //   // );
-      //   return Directionality(
-      //     textDirection: TextDirection.ltr,
-      //     child: Stack(
-      //       children: [
-      //         child,
-      //         const Positioned.fill(
-      //           child: Hello(
-      //             child: RepaintBoundary(
-      //               child: ColoredBox(color: Colors.green),
-      //             ),
-      //           ),
-      //         ),
-      //       ],
-      //     ),
-      //   );
-      // },
       child: widget.child,
     );
   }
 }
-
-class Hello extends SingleChildRenderObjectWidget {
-  const Hello({super.key, super.child});
-
-  @override
-  RenderHello createRenderObject(BuildContext context) => RenderHello();
-
-  @override
-  void updateRenderObject(BuildContext context, RenderHello renderObject) {}
-}
-
-class RenderHello extends RenderProxyBox {}
 
 class _EnterPageAnimationFastByAnimationInner extends StatefulWidget {
   final Widget child;
@@ -245,84 +157,6 @@ class _EnterPageAnimationFastByAnimationInnerState
   }
 }
 
-class _EnterPageAnimationFastByBuilder extends StatefulWidget {
-  final Widget child;
-
-  const _EnterPageAnimationFastByBuilder({required this.child});
-
-  @override
-  State<_EnterPageAnimationFastByBuilder> createState() =>
-      _EnterPageAnimationFastByBuilderState();
-}
-
-class _EnterPageAnimationFastByBuilderState
-    extends State<_EnterPageAnimationFastByBuilder> {
-  final counter = Counter();
-  final animation = _SimpleAnimation();
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (_, constraints) => PreemptBuilder(
-        builder: (_, child) {
-          counter.inc();
-          animation.init();
-          final ratio = animation.computeRatio();
-          // print('$runtimeType PreemptBuilder.builder called ratio=$ratio');
-
-          if (ratio < 1) {
-            SchedulerBinding.instance.addPostFrameCallback((_) {
-              // print(
-              //     '$runtimeType.build addPostFrameCallback callback call setState');
-              setState(() {});
-            });
-          }
-
-          return Directionality(
-            textDirection: TextDirection.ltr,
-            child: Stack(
-              children: [
-                Positioned(
-                  left: constraints.maxWidth * max(0, 1 - ratio),
-                  top: 0,
-                  bottom: 0,
-                  width: constraints.maxWidth,
-                  child: child,
-                ),
-                Center(child: counter.build()),
-                // Center(
-                //   child: Container(
-                //     width: 100,
-                //     height: 100,
-                //     color: Colors
-                //         .primaries[ratio.hashCode % Colors.primaries.length],
-                //   ),
-                // ),
-              ],
-            ),
-          );
-        },
-        child: widget.child,
-      ),
-    );
-  }
-}
-
-// hacky, just b/c it is prototype
-// TODO use vsync, duration, etc
-class _SimpleAnimation {
-  DateTime? initialTime;
-
-  void init() {
-    initialTime ??= DateTime.now();
-  }
-
-  double computeRatio() {
-    return DateTime.now().difference(initialTime!).inMicroseconds /
-        _kDuration.inMicroseconds;
-  }
-}
-
 class Counter {
   var count = 0;
 
@@ -333,29 +167,6 @@ class Counter {
         style: const TextStyle(fontSize: 30, color: Colors.black),
       );
 }
-
-// const invertColorFilter = ColorFilter.matrix(<double>[
-//   -1,
-//   0,
-//   0,
-//   0,
-//   255,
-//   0,
-//   -1,
-//   0,
-//   0,
-//   255,
-//   0,
-//   0,
-//   -1,
-//   0,
-//   255,
-//   0,
-//   0,
-//   0,
-//   1,
-//   0
-// ]);
 
 void printWrapped(String text) =>
     RegExp('.{1,800}').allMatches(text).map((m) => m.group(0)).forEach(print);
