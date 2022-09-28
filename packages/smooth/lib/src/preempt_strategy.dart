bfd4bb82a0bdd89d06bb4ff61150c7a4d1443c3d
fzyzcjy <ch271828n@outlook.com>
2022-09-28 11:38:24 +0800

    Refactor PreemptStrategyNormal, and remove need for lastVsyncInfo (not done) #31

--- packages/smooth/lib/src/preempt_strategy.dart
+++ packages/smooth/lib/src/preempt_strategy.dart
@@ -21,14 +21,11 @@ class _PreemptStrategyNormal implements PreemptStrategy {
 
   _PreemptStrategyNormal();
 
+  // this threshold is not sensitive. see design doc.
+  static const kThreshUs = 2 * 1000;
+
   @override
   bool shouldAct() {
-    final binding = WidgetsFlutterBinding.ensureInitialized();
-
-    // e.g. set to 1ms
-    // this threshold is not sensitive. see design doc.
-    const kThreshUs = 2 * 1000;
-
     diffDateTimeTimePoint ??= binding.lastVsyncInfo().diffDateTimeTimePoint;
 
     // TODO things below can also be cached
@@ -36,15 +33,6 @@ class _PreemptStrategyNormal implements PreemptStrategy {
     // look at source code, that timestamp is indeed VsyncTargetTime
     final lastJankFrameVsyncTargetTime =
         binding.currentSystemFrameTimeStamp.inMicroseconds;
-    // final lastPreemptFrameVsyncTargetTime =
-    //     lastVsyncInfoWhenPreviousPreemptRender!
-    //         .vsyncTargetTimeRaw.inMicroseconds;
-    // final interestVsyncTargetTime =
-    //     max(lastJankFrameVsyncTargetTime, lastPreemptFrameVsyncTargetTime);
-    // final interestVsyncTargetDateTimeUs = interestVsyncTargetTime +
-    //     lastVsyncInfoWhenPreviousPreemptRender!.diffDateTimeTimePoint;
-    // final interestNextVsyncTargetDateTimeUs =
-    //     interestVsyncTargetDateTimeUs + 1000000 ~/ 60;
 
     final interestVsyncTargetTime = max(lastJankFrameVsyncTargetTime,
         interestVsyncTargetTimeByLastPreemptRender);
@@ -68,14 +56,10 @@ class _PreemptStrategyNormal implements PreemptStrategy {
 
   @override
   Duration get currentVsyncTargetTime =>
-      // TODO dup call to `lastVsyncInfo` here
-      SchedulerBinding.instance.lastVsyncInfo().vsyncTargetTimeAdjusted;
+      lastVsyncInfo.vsyncTargetTimeAdjusted;
 
   @override
   void onPreemptRender() {
-    // NOTE this may be slow; and has duplicate call here
-    final lastVsyncInfo = SchedulerBinding.instance.lastVsyncInfo();
-
     final now = DateTime.now();
 
     final shouldShiftOneFrameForInterestVsyncTarget =
@@ -90,6 +74,83 @@ class _PreemptStrategyNormal implements PreemptStrategy {
 
   static const _kOneFrameUs = 1000000 ~/ 60;
 }
+
+// #31 changes it
+// class _PreemptStrategyNormal implements PreemptStrategy {
+//   int? diffDateTimeTimePoint;
+//   var interestVsyncTargetTimeByLastPreemptRender = 0;
+//
+//   _PreemptStrategyNormal();
+//
+//   @override
+//   bool shouldAct() {
+//     final binding = WidgetsFlutterBinding.ensureInitialized();
+//
+//     // e.g. set to 1ms
+//     // this threshold is not sensitive. see design doc.
+//     const kThreshUs = 2 * 1000;
+//
+//     diffDateTimeTimePoint ??= binding.lastVsyncInfo().diffDateTimeTimePoint;
+//
+//     // TODO things below can also be cached
+//
+//     // look at source code, that timestamp is indeed VsyncTargetTime
+//     final lastJankFrameVsyncTargetTime =
+//         binding.currentSystemFrameTimeStamp.inMicroseconds;
+//     // final lastPreemptFrameVsyncTargetTime =
+//     //     lastVsyncInfoWhenPreviousPreemptRender!
+//     //         .vsyncTargetTimeRaw.inMicroseconds;
+//     // final interestVsyncTargetTime =
+//     //     max(lastJankFrameVsyncTargetTime, lastPreemptFrameVsyncTargetTime);
+//     // final interestVsyncTargetDateTimeUs = interestVsyncTargetTime +
+//     //     lastVsyncInfoWhenPreviousPreemptRender!.diffDateTimeTimePoint;
+//     // final interestNextVsyncTargetDateTimeUs =
+//     //     interestVsyncTargetDateTimeUs + 1000000 ~/ 60;
+//
+//     final interestVsyncTargetTime = max(lastJankFrameVsyncTargetTime,
+//         interestVsyncTargetTimeByLastPreemptRender);
+//
+//     final interestVsyncTargetDateTimeUs =
+//         interestVsyncTargetTime + diffDateTimeTimePoint!;
+//
+//     final nowDateTimeUs = DateTime.now().microsecondsSinceEpoch;
+//
+//     final ans = nowDateTimeUs > interestVsyncTargetDateTimeUs - kThreshUs;
+//
+//     // if (ans) {
+//     //   print('shouldAct=true '
+//     //       'now=${DateTime.fromMicrosecondsSinceEpoch(nowDateTimeUs)} '
+//     //       'interestVsyncTargetDateTimeUs=${DateTime.fromMicrosecondsSinceEpoch(interestVsyncTargetDateTimeUs)} '
+//     //       'maybePreemptRenderCallCount=$_maybePreemptRenderCallCount');
+//     // }
+//
+//     return ans;
+//   }
+//
+//   @override
+//   Duration get currentVsyncTargetTime =>
+//       // TODO dup call to `lastVsyncInfo` here
+//       SchedulerBinding.instance.lastVsyncInfo().vsyncTargetTimeAdjusted;
+//
+//   @override
+//   void onPreemptRender() {
+//     // NOTE this may be slow; and has duplicate call here
+//     final lastVsyncInfo = SchedulerBinding.instance.lastVsyncInfo();
+//
+//     final now = DateTime.now();
+//
+//     final shouldShiftOneFrameForInterestVsyncTarget =
+//         now.difference(lastVsyncInfo.vsyncTargetDateTime) >
+//             const Duration(milliseconds: -4);
+//
+//     diffDateTimeTimePoint = lastVsyncInfo.diffDateTimeTimePoint;
+//     interestVsyncTargetTimeByLastPreemptRender =
+//         lastVsyncInfo.vsyncTargetTimeRaw.inMicroseconds +
+//             (shouldShiftOneFrameForInterestVsyncTarget ? _kOneFrameUs : 0);
+//   }
+//
+//   static const _kOneFrameUs = 1000000 ~/ 60;
+// }
 
 class _PreemptStrategyNever implements PreemptStrategy {
   const _PreemptStrategyNever();
