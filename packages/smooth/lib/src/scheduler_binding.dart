415b4f8699303c6531f840b2107a729435427954
fzyzcjy <ch271828n@outlook.com>
2022-09-28 18:21:04 +0800

    change  SmoothSchedulerBindingMixin currentFrameVsyncTargetTime to beginFrameDateTime, because the former is right on real device but wrong on test env #32

--- packages/smooth/lib/src/scheduler_binding.dart
+++ packages/smooth/lib/src/scheduler_binding.dart
@@ -4,31 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
 mixin SmoothSchedulerBindingMixin on SchedulerBinding {
-  /// The [DateTime] for [currentFrameVsyncTargetTimeStamp]
-  DateTime get currentFrameVsyncTargetTime => _currentFrameVsyncTargetTime!;
-  DateTime? _currentFrameVsyncTargetTime;
-
-  /// The current VsyncTargetTime
-  // p.s. Look at source code, we see:
-  // 1. [currentSystemFrameTimeStamp] is VsyncTargetTime
-  // 2. [currentFrameTimeStamp] is the adjusted [currentSystemFrameTimeStamp]
-  // 3. [currentFrameTimeStamp] is provided to animation callbacks
-  Duration get currentFrameVsyncTargetTimeStamp => currentFrameTimeStamp;
-
-  int get diffDateTimeToTimeStamp =>
-      currentFrameVsyncTargetTime.microsecondsSinceEpoch -
-      currentFrameVsyncTargetTimeStamp.inMicroseconds;
+  DateTime get beginFrameDateTime => _beginFrameDateTime!;
+  DateTime? _beginFrameDateTime;
 
   @override
   void handleBeginFrame(Duration? rawTimeStamp) {
-    // NOTE off-by-one problem
-    // By tracing code, [rawTimeStamp] is indeed [VsyncTargetTime],
-    // usually the end of this frame.
-    // But [clock.now()] is begin of this frame.
-    // So we add 16ms to clock.now.
-    // #31
-    _currentFrameVsyncTargetTime = clock.now().add(kOneFrame);
-
+    _beginFrameDateTime = clock.now();
     super.handleBeginFrame(rawTimeStamp);
   }
 
