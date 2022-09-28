90c948c97a4894dcc4471847c60a85d2b06348ca
fzyzcjy <ch271828n@outlook.com>
2022-09-28 18:22:20 +0800

    naive SmoothSchedulerBindingMixin test #32

--- packages/smooth/test/src/scheduler_binding_test.dart
+++ packages/smooth/test/src/scheduler_binding_test.dart
@@ -1,53 +1,19 @@
 import 'package:clock/clock.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:smooth/src/scheduler_binding.dart';
 
 import 'test_tools/binding.dart';
-import 'test_tools/widgets.dart';
 
 void main() {
   final binding = SmoothAutomatedTestWidgetsFlutterBinding.ensureInitialized();
 
   group('SmoothSchedulerBindingMixin', () {
-    testWidgets('currentFrameVsyncTargetTime and diffDateTimeToTimeStamp',
-        (tester) async {
-      TODO_the_values_are_wrong;
-
-      final startTime = clock.now();
-
-      final onBuild = OnceCallback();
-
-      final expectDiffDateTimeToTimeStamp = startTime.microsecondsSinceEpoch +
-          1000000 +
-          SmoothSchedulerBindingMixin.kOneFrameUs;
-
-      onBuild.value = () {
-        expect(
-          binding.currentFrameVsyncTargetTime,
-          startTime
-              .add(const Duration(milliseconds: 1000, microseconds: 16666)),
-        );
-        expect(binding.diffDateTimeToTimeStamp, expectDiffDateTimeToTimeStamp);
-      };
-
-      await tester.pumpWidget(
-        AlwaysBuildBuilder(onBuild: onBuild),
-        const Duration(milliseconds: 1000),
-      );
-
-      onBuild.value = () {
-        expect(
-          binding.currentFrameVsyncTargetTime,
-          startTime
-              .add(const Duration(milliseconds: 1100, microseconds: 16666)),
-        );
-        expect(binding.diffDateTimeToTimeStamp, expectDiffDateTimeToTimeStamp);
-      };
-
-      await tester.pump(const Duration(milliseconds: 100));
-
-      expect(onBuild.isEmpty, true);
+    testWidgets('beginFrameDateTime', (tester) async {
+      // very naive test, because clock has not even run for a millisecond
+      // during this whole test!
+      final expectBeginFrameDateTime = clock.now();
+      await tester.pumpWidget(Container());
+      expect(binding.beginFrameDateTime, expectBeginFrameDateTime);
     });
   });
 }
