61629a37fb6cbb248cd3224f092f1accfd80a086
fzyzcjy <ch271828n@outlook.com>
2022-09-29 20:36:32 +0800

    revert ComplexWidget #55

--- packages/smooth/example/lib/utils/complex_widget.dart
+++ packages/smooth/example/lib/utils/complex_widget.dart
@@ -3,8 +3,8 @@
 import 'package:flutter/material.dart';
 import 'package:smooth/smooth.dart';
 
-class ComplexLongColumn extends StatelessWidget {
-  const ComplexLongColumn({super.key});
+class ComplexWidget extends StatelessWidget {
+  const ComplexWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +12,9 @@ class ComplexLongColumn extends StatelessWidget {
     // const N = 60; // make it big to see jank clearly
     const N = 150; // make it big to see jank clearly
     // const N = 1000; // for debug
+
+    // const textRepeat = 5;
+    const textRepeat = 1;
 
     // do not let semantics confuse the metrics. b/c we are having a huge
     // amount of text in this demo, while in realworld never has that
@@ -23,51 +26,34 @@ class ComplexLongColumn extends StatelessWidget {
           alignment: Alignment.topCenter,
           maxHeight: double.infinity,
           child: Column(
-            children: List<Widget>.generate(N, (index) {
-              return SmoothPreemptPoint(
-                child: ComplexListTile(index: index),
+            children: List<Widget>.generate(N, (int index) {
+              return SizedBox(
+                height: 12,
+                // NOTE hack, in real world should auto have preempt point
+                // but in prototype we do it by hand
+                child: SmoothPreemptPoint(
+                  child: ListTile(
+                    dense: true,
+                    visualDensity: VisualDensity.compact,
+                    leading: SizedBox(
+                      width: 16,
+                      height: 16,
+                      child: CircleAvatar(
+                        child: Text('G$index'),
+                      ),
+                    ),
+                    title: Text(
+                      'Foo contact from $index-th local contact' * textRepeat,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(fontSize: 5),
+                    ),
+                    subtitle: Text('+91 88888 8800$index'),
+                  ),
+                ),
               );
             }),
           ),
         ),
-      ),
-    );
-  }
-}
-
-class ComplexListTile extends StatelessWidget {
-  final int index;
-
-  const ComplexListTile({super.key, required this.index});
-
-  static void Function(int index)? onBuild;
-
-  // static const _textRepeat = 5;
-  static const _textRepeat = 1;
-
-  @override
-  Widget build(BuildContext context) {
-    onBuild?.call(index);
-    TODO_need_on_layout;
-
-    return SizedBox(
-      height: 12,
-      child: ListTile(
-        dense: true,
-        visualDensity: VisualDensity.compact,
-        leading: SizedBox(
-          width: 16,
-          height: 16,
-          child: CircleAvatar(
-            child: Text('G$index'),
-          ),
-        ),
-        title: Text(
-          'Foo contact from $index-th local contact' * _textRepeat,
-          overflow: TextOverflow.ellipsis,
-          style: const TextStyle(fontSize: 5),
-        ),
-        subtitle: Text('+91 88888 8800$index'),
       ),
     );
   }
