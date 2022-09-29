d41b0d0ab4f021beabdb708c54e01c772a792cb3
fzyzcjy <ch271828n@outlook.com>
2022-09-29 11:20:20 +0800

    add ProxyPipelineOwner #52

---
+++ packages/smooth/lib/src/proxy.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/rendering.dart';
+
+class ProxyPipelineOwner implements PipelineOwner {
+  final PipelineOwner inner;
+
+  ProxyPipelineOwner(this.inner);
+
+  @override
+  VoidCallback? get onNeedVisualUpdate => inner.onNeedVisualUpdate;
+
+  @override
+  VoidCallback? get onSemanticsOwnerCreated => inner.onSemanticsOwnerCreated;
+
+  @override
+  VoidCallback? get onSemanticsOwnerDisposed => inner.onSemanticsOwnerDisposed;
+
+  @override
+  void requestVisualUpdate() => inner.requestVisualUpdate();
+
+  @override
+  AbstractNode? get rootNode => inner.rootNode;
+
+  @override
+  set rootNode(AbstractNode? value) => inner.rootNode = value;
+
+  @override
+  bool get debugDoingLayout => inner.debugDoingLayout;
+
+  @override
+  void flushLayout() => inner.flushLayout();
+
+  @override
+  void flushCompositingBits() => inner.flushCompositingBits();
+
+  @override
+  bool get debugDoingPaint => inner.debugDoingPaint;
+
+  @override
+  void flushPaint() => inner.flushPaint();
+
+  @override
+  SemanticsOwner? get semanticsOwner => inner.semanticsOwner;
+
+  @override
+  int get debugOutstandingSemanticsHandles =>
+      inner.debugOutstandingSemanticsHandles;
+
+  @override
+  SemanticsHandle ensureSemantics({VoidCallback? listener}) =>
+      inner.ensureSemantics(listener: listener);
+
+  @override
+  void flushSemantics() => inner.flushSemantics();
+}
