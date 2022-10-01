af7117a614a820152f82ce819ea9be82432dec9b
fzyzcjy <ch271828n@outlook.com>
2022-10-01 11:40:36 +0800

    remove debugging widgets #5884

--- packages/smooth/example/lib/example_gesture/example_gesture_page.dart
+++ packages/smooth/example/lib/example_gesture/example_gesture_page.dart
@@ -1,8 +1,7 @@
+import 'package:example/example_gesture/gesture_visualizer.dart';
 import 'package:example/utils/complex_widget.dart';
 import 'package:example/utils/debug_plain_animation.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:smooth/smooth.dart';
 
@@ -19,27 +18,27 @@ class ExampleGesturePage extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
         child: Column(
           children: [
-            const LeaveAfterSomeFrames(),
+            // const LeaveAfterSomeFrames(),
             const RepaintBoundary(
               child: CounterWidget(prefix: 'Plain: '),
             ),
             SizedBox(
               height: 200,
               child: SmoothBuilder(
-                // TODO only to reproduce #5879
-                builder: (_, child) => const _Dummy(),
-                // builder: (_, child) => Directionality(
-                //   textDirection: TextDirection.ltr,
-                //   child: GestureVisualizerByListener(
-                //     child: Stack(
-                //       children: [
-                //         Positioned.fill(
-                //             child: ColoredBox(color: Colors.blue.shade50)),
-                //         const CounterWidget(prefix: 'Smooth: '),
-                //       ],
-                //     ),
-                //   ),
-                // ),
+                // // only to reproduce #5879
+                // builder: (_, child) => const _Dummy(),
+                builder: (_, child) => Directionality(
+                  textDirection: TextDirection.ltr,
+                  child: GestureVisualizerByListener(
+                    child: Stack(
+                      children: [
+                        Positioned.fill(
+                            child: ColoredBox(color: Colors.blue.shade50)),
+                        const CounterWidget(prefix: 'Smooth: '),
+                      ],
+                    ),
+                  ),
+                ),
                 // child: Container(color: Colors.green),
                 child: const SizedBox(),
               ),
@@ -80,102 +79,102 @@ class ExampleGesturePage extends StatelessWidget {
   }
 }
 
-class _Dummy extends StatefulWidget {
-  const _Dummy();
-
-  @override
-  State<_Dummy> createState() => _DummyState();
-}
-
-class _DummyState extends State<_Dummy> {
-  var count = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    count++;
-    SchedulerBinding.instance.addPostFrameCallback((_) {
-      if (!mounted) return;
-      setState(() {});
-    });
-
-    print('hi ${describeIdentity(this)}.build');
-    return _DummyInner(
-      dummy: count,
-      child: ColoredBox(color: Colors.cyan[(count % 8 + 1) * 100]!),
-    );
-  }
-}
-
-class _DummyInner extends SingleChildRenderObjectWidget {
-  final int dummy;
-
-  const _DummyInner({
-    super.key,
-    required this.dummy,
-    super.child,
-  });
-
-  @override
-  _RenderDummy createRenderObject(BuildContext context) =>
-      _RenderDummy(dummy: dummy);
-
-  @override
-  void updateRenderObject(BuildContext context, _RenderDummy renderObject) {
-    renderObject.dummy = dummy;
-  }
-}
-
-class _RenderDummy extends RenderProxyBox {
-  _RenderDummy({
-    required int dummy,
-    RenderBox? child,
-  })  : _dummy = dummy,
-        super(child);
-
-  // not mark repaint yet
-  int get dummy => _dummy;
-  int _dummy;
-
-  set dummy(int value) {
-    if (_dummy == value) return;
-    _dummy = value;
-    print('hi ${describeIdentity(this)} set dummy thus markNeedsPaint START');
-    markNeedsPaint();
-    print('hi ${describeIdentity(this)} set dummy thus markNeedsPaint END');
-  }
-
-  @override
-  void paint(PaintingContext context, Offset offset) {
-    print('hi ${describeIdentity(this)}.paint');
-    super.paint(context, offset);
-  }
-}
-
-class LeaveAfterSomeFrames extends StatefulWidget {
-  final VoidCallback? onBuild;
-  final Widget? child;
-
-  const LeaveAfterSomeFrames({super.key, this.onBuild, this.child});
-
-  @override
-  State<LeaveAfterSomeFrames> createState() => _LeaveAfterSomeFramesState();
-}
-
-class _LeaveAfterSomeFramesState extends State<LeaveAfterSomeFrames> {
-  var count = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    count++;
-    SchedulerBinding.instance.addPostFrameCallback((_) {
-      if (mounted) setState(() {});
-      if (count >= 10) {
-        print('action: Leave after some frames!');
-        Navigator.pop(context);
-        count = -99999;
-      }
-    });
-    widget.onBuild?.call();
-    return widget.child ?? Container();
-  }
-}
+// class _Dummy extends StatefulWidget {
+//   const _Dummy();
+//
+//   @override
+//   State<_Dummy> createState() => _DummyState();
+// }
+//
+// class _DummyState extends State<_Dummy> {
+//   var count = 0;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     count++;
+//     SchedulerBinding.instance.addPostFrameCallback((_) {
+//       if (!mounted) return;
+//       setState(() {});
+//     });
+//
+//     print('hi ${describeIdentity(this)}.build');
+//     return _DummyInner(
+//       dummy: count,
+//       child: ColoredBox(color: Colors.cyan[(count % 8 + 1) * 100]!),
+//     );
+//   }
+// }
+//
+// class _DummyInner extends SingleChildRenderObjectWidget {
+//   final int dummy;
+//
+//   const _DummyInner({
+//     super.key,
+//     required this.dummy,
+//     super.child,
+//   });
+//
+//   @override
+//   _RenderDummy createRenderObject(BuildContext context) =>
+//       _RenderDummy(dummy: dummy);
+//
+//   @override
+//   void updateRenderObject(BuildContext context, _RenderDummy renderObject) {
+//     renderObject.dummy = dummy;
+//   }
+// }
+//
+// class _RenderDummy extends RenderProxyBox {
+//   _RenderDummy({
+//     required int dummy,
+//     RenderBox? child,
+//   })  : _dummy = dummy,
+//         super(child);
+//
+//   // not mark repaint yet
+//   int get dummy => _dummy;
+//   int _dummy;
+//
+//   set dummy(int value) {
+//     if (_dummy == value) return;
+//     _dummy = value;
+//     print('hi ${describeIdentity(this)} set dummy thus markNeedsPaint START');
+//     markNeedsPaint();
+//     print('hi ${describeIdentity(this)} set dummy thus markNeedsPaint END');
+//   }
+//
+//   @override
+//   void paint(PaintingContext context, Offset offset) {
+//     print('hi ${describeIdentity(this)}.paint');
+//     super.paint(context, offset);
+//   }
+// }
+//
+// class LeaveAfterSomeFrames extends StatefulWidget {
+//   final VoidCallback? onBuild;
+//   final Widget? child;
+//
+//   const LeaveAfterSomeFrames({super.key, this.onBuild, this.child});
+//
+//   @override
+//   State<LeaveAfterSomeFrames> createState() => _LeaveAfterSomeFramesState();
+// }
+//
+// class _LeaveAfterSomeFramesState extends State<LeaveAfterSomeFrames> {
+//   var count = 0;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     count++;
+//     SchedulerBinding.instance.addPostFrameCallback((_) {
+//       if (mounted) setState(() {});
+//       if (count >= 10) {
+//         print('action: Leave after some frames!');
+//         Navigator.pop(context);
+//         count = -99999;
+//       }
+//     });
+//     widget.onBuild?.call();
+//     return widget.child ?? Container();
+//   }
+// }
