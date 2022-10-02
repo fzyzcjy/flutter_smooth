aa9d9d5fa21976f75db8d0735a629658ce1b7206
fzyzcjy <ch271828n@outlook.com>
2022-10-02 14:00:10 +0800

    refactor #5912

--- packages/smooth/example/lib/example_list_view/example_list_view_page.dart
+++ packages/smooth/example/lib/example_list_view/example_list_view_page.dart
@@ -18,39 +18,43 @@ class _ExampleListViewPageState extends State<ExampleListViewPage> {
         title: const Text('Example'),
       ),
       body: ListView.builder(
-        itemBuilder: (_, index) => SmoothPreemptPoint(
-          child: ListTile(
-            dense: true,
-            visualDensity: VisualDensity.compact,
-            leading: SizedBox(
-              width: 32,
-              height: 32,
-              child: CircleAvatar(
-                child: Text(
-                  'G$index',
-                  style: const TextStyle(fontSize: 12),
-                ),
-              ),
-            ),
-            title: Text(
-              'Foo contact from $index-th local contact',
-              overflow: TextOverflow.ellipsis,
-            ),
-            subtitle: Stack(
-              children: [
-                // simulate slow build/layout; do not paint it, since much more
-                // than realistic number of text
-                Opacity(
-                  opacity: 0,
-                  child: Text(
-                    '+91 88888 8800$index ' * 120,
-                    style: const TextStyle(fontSize: 3),
-                  ),
-                ),
-                Text('subtitle\n' * (3 + Random().nextInt(3))),
-              ],
+        itemBuilder: _buildRow,
+      ),
+    );
+  }
+
+  Widget _buildRow(BuildContext context, int index) {
+    return SmoothPreemptPoint(
+      child: ListTile(
+        dense: true,
+        visualDensity: VisualDensity.compact,
+        leading: SizedBox(
+          width: 32,
+          height: 32,
+          child: CircleAvatar(
+            child: Text(
+              'G$index',
+              style: const TextStyle(fontSize: 12),
             ),
           ),
+        ),
+        title: Text(
+          'Foo contact from $index-th local contact',
+          overflow: TextOverflow.ellipsis,
+        ),
+        subtitle: Stack(
+          children: [
+            // simulate slow build/layout; do not paint it, since much more
+            // than realistic number of text
+            Opacity(
+              opacity: 0,
+              child: Text(
+                '+91 88888 8800$index ' * 120,
+                style: const TextStyle(fontSize: 3),
+              ),
+            ),
+            Text('subtitle\n' * (3 + Random().nextInt(3))),
+          ],
         ),
       ),
     );
