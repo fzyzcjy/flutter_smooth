1a8edc4fff73b8e1b06207f0b193082639656831
fzyzcjy <ch271828n@outlook.com>
2022-10-03 20:10:03 +0800

    minor #5917

--- packages/smooth/example/lib/example_list_view/example_list_view_page.dart
+++ packages/smooth/example/lib/example_list_view/example_list_view_page.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:example/utils/debug_plain_animation.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth/smooth.dart';
 
@@ -19,7 +20,12 @@ class _ExampleListViewPageState extends State<ExampleListViewPage> {
       appBar: AppBar(
         title: Text('Example (${widget.enableSmooth ? 'smooth' : 'plain'})'),
       ),
-      body: widget.enableSmooth ? _buildSmooth() : _buildPlain(),
+      body: Column(
+        children: [
+          const CounterWidget(prefix: 'Plain: '),
+          Expanded(child: widget.enableSmooth ? _buildSmooth() : _buildPlain()),
+        ],
+      ),
     );
   }
 
@@ -41,38 +47,47 @@ class _ExampleListViewPageState extends State<ExampleListViewPage> {
   }
 
   Widget _buildRow(BuildContext context, int index) {
-    return SmoothPreemptPoint(
-      child: ListTile(
-        dense: true,
-        visualDensity: VisualDensity.compact,
-        leading: SizedBox(
-          width: 32,
-          height: 32,
-          child: CircleAvatar(
-            child: Text(
-              'G$index',
-              style: const TextStyle(fontSize: 12),
-            ),
+    // const workload = 10;
+    const workload = 200;
+
+    return ListTile(
+      dense: true,
+      visualDensity: VisualDensity.compact,
+      leading: SizedBox(
+        width: 32,
+        height: 32,
+        child: CircleAvatar(
+          child: Text(
+            'G$index',
+            style: const TextStyle(fontSize: 12),
           ),
         ),
-        title: Text(
-          'Foo contact from $index-th local contact',
-          overflow: TextOverflow.ellipsis,
-        ),
-        subtitle: Stack(
-          children: [
-            // simulate slow build/layout; do not paint it, since much more
-            // than realistic number of text
-            Opacity(
-              opacity: 0,
-              child: Text(
-                '+91 88888 8800$index ' * 120,
-                style: const TextStyle(fontSize: 3),
+      ),
+      title: Text(
+        'Foo contact from $index-th local contact',
+        overflow: TextOverflow.ellipsis,
+      ),
+      subtitle: Stack(
+        children: [
+          for (var i = 0; i < workload; ++i)
+            SmoothPreemptPoint(
+              child: SizedBox(
+                height: 36,
+                child: OverflowBox(
+                  // simulate slow build/layout; do not paint it, since much more
+                  // than realistic number of text
+                  child: Opacity(
+                    opacity: 0,
+                    child: Text(
+                      '+91 88888 8800$index ' * 100,
+                      style: const TextStyle(fontSize: 3),
+                    ),
+                  ),
+                ),
               ),
             ),
-            Text('subtitle\n' * (3 + Random().nextInt(3))),
-          ],
-        ),
+          Text('subtitle\n' * (3 + Random().nextInt(3))),
+        ],
       ),
     );
   }
--- packages/smooth/example/lib/utils/debug_plain_animation.dart
+++ packages/smooth/example/lib/utils/debug_plain_animation.dart
@@ -61,36 +61,47 @@ class _CounterWidgetState extends State<CounterWidget>
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        AnimatedBuilder(
-          animation: _controller,
-          builder: (_, __) {
-            // print(
-            //     '$runtimeType AnimatedBuilder.builder called (${widget.prefix})');
-            _count++;
-            return Text(
-              '${widget.prefix} ${_count.toString().padRight(10)} ${DateTime.now()}',
-              style: const TextStyle(color: Colors.black, fontSize: 22),
-            );
-          },
-        ),
-        RotationTransition(
-          turns: _controller,
-          child: Container(
-            width: 50,
-            height: 50,
-            color: Colors.green,
-            child: const Center(
-              child: Text(
-                'a',
-                style: TextStyle(fontSize: 50, color: Colors.white, height: 1),
+    return SizedBox(
+      height: 36,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Center(
+              child: AnimatedBuilder(
+                animation: _controller,
+                builder: (_, __) {
+                  // print(
+                  //     '$runtimeType AnimatedBuilder.builder called (${widget.prefix})');
+                  _count++;
+                  return Text(
+                    '${widget.prefix} ${_count.toString().padRight(10)} ${DateTime.now()}',
+                    style: const TextStyle(color: Colors.black, fontSize: 18),
+                  );
+                },
               ),
             ),
           ),
-        ),
-      ],
+          Positioned.fill(
+            child: Center(
+              child: RotationTransition(
+                turns: _controller,
+                child: Container(
+                  width: 36,
+                  height: 36,
+                  color: Colors.green,
+                  child: const Center(
+                    child: Text(
+                      'a',
+                      style: TextStyle(
+                          fontSize: 36, color: Colors.white, height: 1),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
