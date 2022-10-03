631075d21af97927c54516b9ffb9174926158b6e
fzyzcjy <ch271828n@outlook.com>
2022-10-03 20:32:03 +0800

    fix bug of schedule too many addPostMainTreeFlushLayoutCallback #5917

--- packages/smooth/lib/src/shift.dart
+++ packages/smooth/lib/src/shift.dart
@@ -11,24 +11,34 @@ class SmoothShift extends StatefulWidget {
 }
 
 class _SmoothShiftState extends State<SmoothShift> {
-  var offset = 0.0;
+  var _offset = 0.0;
+
+  var _hasPendingCallback = false;
+
+  void _maybeSchedulePostMainTreeFlushLayoutCallback() {
+    if (_hasPendingCallback) return;
+
+    SmoothSchedulerBindingMixin.instance.addPostMainTreeFlushLayoutCallback(() {
+      print('hi $runtimeType addPostMainTreeFlushLayoutCallback.callback');
+
+      _hasPendingCallback = false;
+
+      if (_offset == 0) return;
+      setState(() => _offset = 0);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    print('hi $runtimeType build offset=$offset');
-
-    // TODO should we put it in `build`?
-    SmoothSchedulerBindingMixin.instance.addPostMainTreeFlushLayoutCallback(() {
-      print('hi $runtimeType addPostMainTreeFlushLayoutCallback.callback');
-      if (offset == 0) return;
-      setState(() => offset = 0);
-    });
+    print('hi $runtimeType build offset=$_offset');
+   
+    _maybeSchedulePostMainTreeFlushLayoutCallback();
 
     return Listener(
       onPointerMove: _handlePointerMove,
       behavior: HitTestBehavior.translucent,
       child: Transform.translate(
-        offset: Offset(0, offset),
+        offset: Offset(0, _offset),
         child: widget.child,
       ),
     );
@@ -39,7 +49,7 @@ class _SmoothShiftState extends State<SmoothShift> {
     setState(() {
       // very naive, and is WRONG!
       // just to confirm, we can (1) receive (2) display events
-      offset += e.localDelta.dy;
+      _offset += e.localDelta.dy;
     });
   }
 }
