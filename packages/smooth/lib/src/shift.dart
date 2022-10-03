2484818c93c108e5ca4231396b9607eee55a982d
fzyzcjy <ch271828n@outlook.com>
2022-10-03 19:58:27 +0800

    most naive version of SmoothShift #5917

--- packages/smooth/lib/src/shift.dart
+++ packages/smooth/lib/src/shift.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
-class SmoothShift extends StatelessWidget {
+class SmoothShift extends StatefulWidget {
   final Widget child;
 
   const SmoothShift({super.key, required this.child});
 
   @override
+  State<SmoothShift> createState() => _SmoothShiftState();
+}
+
+class _SmoothShiftState extends State<SmoothShift> {
+  var offset = Offset.zero;
+
+  @override
   Widget build(BuildContext context) {
-    // TODO
-    return child;
+    return Listener(
+      onPointerMove: _handlePointerMove,
+      child: Transform.translate(
+        offset: offset,
+        child: widget.child,
+      ),
+    );
+  }
+
+  void _handlePointerMove(PointerMoveEvent e) {
+    setState(() {
+      // very naive, and is WRONG!
+      // just to confirm, we can (1) receive (2) display events
+      offset += e.localDelta;
+    });
   }
 }
