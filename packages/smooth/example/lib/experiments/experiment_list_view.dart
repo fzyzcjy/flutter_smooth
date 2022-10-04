8a5f9d016ef67fce81f24bbe2401a1ed16f15bd6
fzyzcjy <ch271828n@outlook.com>
2022-10-04 14:40:44 +0800

    extend ScrollController #5918

--- packages/smooth/example/lib/experiments/experiment_list_view.dart
+++ packages/smooth/example/lib/experiments/experiment_list_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class ExperimentListView extends StatefulWidget {
@@ -8,16 +9,70 @@ class ExperimentListView extends StatefulWidget {
 }
 
 class _ExperimentListViewState extends State<ExperimentListView> {
+  final controller = _MyScrollController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: ListView.builder(
+          controller: controller,
           itemBuilder: (_, index) => ListTile(
             title: Text('i=$index'),
           ),
         ),
       ),
     );
+  }
+}
+
+class _MyScrollController extends ScrollController {
+  @override
+  ScrollPosition createScrollPosition(
+    ScrollPhysics physics,
+    ScrollContext context,
+    ScrollPosition? oldPosition,
+  ) {
+    // ref [super.createScrollPosition]
+    return _MyScrollPositionWithSingleContext(
+      physics: physics,
+      context: context,
+      initialPixels: initialScrollOffset,
+      keepScrollOffset: keepScrollOffset,
+      oldPosition: oldPosition,
+      debugLabel: debugLabel,
+    );
+  }
+}
+
+class _MyScrollPositionWithSingleContext
+    extends ScrollPositionWithSingleContext {
+  _MyScrollPositionWithSingleContext({
+    required super.physics,
+    required super.context,
+    super.initialPixels,
+    super.keepScrollOffset,
+    super.oldPosition,
+    super.debugLabel,
+  });
+
+  @override
+  void goBallistic(double velocity) {
+    // ref [super.createScrollPosition]
+    assert(hasPixels);
+    final simulation = physics.createBallisticSimulation(this, velocity);
+    if (simulation != null) {
+      // TODO do something here?
+      print('hi ${describeIdentity(this)}.goBallistic simulation=$simulation');
+
+      beginActivity(BallisticScrollActivity(
+        this,
+        simulation,
+        context.vsync,
+        activity?.shouldIgnorePointer ?? true,
+      ));
+    } else {
+      goIdle();
+    }
   }
 }
