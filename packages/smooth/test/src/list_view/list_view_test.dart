74e23d4f265032d898bd1936b9e8d6904fef4a70
fzyzcjy <ch271828n@outlook.com>
2022-10-04 19:46:58 +0800

    add tests for #5961, not done

--- packages/smooth/test/src/list_view/list_view_test.dart
+++ packages/smooth/test/src/list_view/list_view_test.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:smooth/smooth.dart';
 import 'package:smooth_dev/smooth_dev.dart';
@@ -41,6 +42,53 @@ void main() {
           ]);
         });
       }
+    });
+
+    group('when user drags ListView, should be smooth', () {
+      testWidgets('simple', (tester) async {
+        debugPrintBeginFrameBanner = debugPrintEndFrameBanner = true;
+        final timeInfo = TimeInfo();
+        final capturer = WindowRenderCapturer.autoDispose();
+
+        await tester.pumpWidget(SmoothScope(
+          child: Directionality(
+            textDirection: TextDirection.ltr,
+            child: SmoothListView.builder(
+              itemCount: 100,
+              itemBuilder: (_, index) => Container(
+                height: 60,
+                color: Colors.primaries[index],
+              ),
+            ),
+          ),
+        ));
+        await capturer
+            .expectAndReset(tester, expectTestFrameNumber: 2, expectImages: [
+          await tester.createScreenImage((im) => im
+            ..fillRect(const Rectangle(0, 0, 50, 60), Colors.primaries[0])
+            ..fillRect(const Rectangle(0, 60, 50, 40), Colors.primaries[1])),
+        ]);
+
+        TODO_simulate_extra_pointer_move_event;
+
+        TODO_simulate_preempt_render;
+
+        await tester.pump(timeInfo.calcPumpDuration(smoothFrameIndex: TODO));
+        await capturer
+            .expectAndReset(tester, expectTestFrameNumber: 3, expectImages: [
+          TODO,
+        ]);
+
+        TODO_the_extra_events_will_be_normal_events;
+
+        await tester.pump(timeInfo.calcPumpDuration(smoothFrameIndex: TODO));
+        await capturer
+            .expectAndReset(tester, expectTestFrameNumber: 3, expectImages: [
+          TODO,
+        ]);
+
+        debugPrintBeginFrameBanner = debugPrintEndFrameBanner = false;
+      });
     });
   });
 }
