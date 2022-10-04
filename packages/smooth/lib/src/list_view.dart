a9c2183f4441908de3c057c51676eaecd72b1691
fzyzcjy <ch271828n@outlook.com>
2022-10-04 15:04:26 +0800

    add startTimeStamp #5918

--- packages/smooth/lib/src/list_view.dart
+++ packages/smooth/lib/src/list_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:smooth/src/builder.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:smooth/smooth.dart';
 import 'package:smooth/src/enhanced_padding.dart';
-import 'package:smooth/src/shift.dart';
 
 class SmoothListView extends StatefulWidget {
   final int itemCount;
@@ -94,10 +94,8 @@ class _SmoothScrollPositionWithSingleContext
     super.debugLabel,
   });
 
-  // why "cloned": because [Simulation]'s doc says, some subclasses will change
-  // state when called, and must only call with monotonic timestamps.
-  Simulation? get lastSimulationCloned => _lastSimulationCloned;
-  Simulation? _lastSimulationCloned;
+  SimulationInfo? get lastSimulation => _lastSimulation;
+  SimulationInfo? _lastSimulation;
 
   // ref [super.createScrollPosition], except for marked regions
   @override
@@ -106,7 +104,15 @@ class _SmoothScrollPositionWithSingleContext
     final simulation = physics.createBallisticSimulation(this, velocity);
     if (simulation != null) {
       // NOTE MODIFIED start
-      _lastSimulationCloned = physics.createBallisticSimulation(this, velocity);
+      // NOTE need to create a *new* simulation, not the old one.
+      //      Because [Simulation]'s doc says, some subclasses will change
+      //      state when called, and must only call with monotonic timestamps.
+      _lastSimulation = SimulationInfo(
+        simulation: physics.createBallisticSimulation(this, velocity)!,
+        // TODO correct? should we "+kOneFrame"?
+        startTimeStamp:
+            SchedulerBinding.instance.currentFrameTimeStamp + kOneFrame,
+      );
       // NOTE MODIFIED end
       beginActivity(BallisticScrollActivity(
         this,
@@ -123,4 +129,17 @@ class _SmoothScrollPositionWithSingleContext
 extension on ScrollableState {
   _SmoothScrollPositionWithSingleContext get positionTyped =>
       position as _SmoothScrollPositionWithSingleContext;
+}
+
+class SimulationInfo {
+  final Simulation simulation;
+
+  /// When the [simulation] is used in the animation in [BallisticScrollActivity],
+  /// what is the start time stamp of that animation.
+  final Duration startTimeStamp;
+
+  const SimulationInfo({
+    required this.simulation,
+    required this.startTimeStamp,
+  });
 }
