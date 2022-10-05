f882357561bbd348e9292ab66524121947b2a045
fzyzcjy <ch271828n@outlook.com>
2022-10-05 08:30:02 +0800

    impl ExampleVsyncPage #5981

--- packages/smooth/example/lib/example_vsync/example_vsync_page.dart
+++ packages/smooth/example/lib/example_vsync/example_vsync_page.dart
@@ -1,20 +1,42 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class ExampleVsyncPage extends StatefulWidget {
+import 'package:example/utils/debug_plain_animation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+class ExampleVsyncPage extends StatelessWidget {
   const ExampleVsyncPage({super.key});
 
-  @override
-  State<ExampleVsyncPage> createState() => _ExampleVsyncPageState();
-}
-
-class _ExampleVsyncPageState extends State<ExampleVsyncPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Example: Vsync'),
       ),
-      body: TODO,
+      body: const _AlwaysRebuildDummyWidget(),
     );
+  }
+}
+
+class _AlwaysRebuildDummyWidget extends StatefulWidget {
+  const _AlwaysRebuildDummyWidget();
+
+  @override
+  State<_AlwaysRebuildDummyWidget> createState() =>
+      _AlwaysRebuildDummyWidgetState();
+}
+
+class _AlwaysRebuildDummyWidgetState extends State<_AlwaysRebuildDummyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    // deliberately make each frame a bit more than 16.67ms
+    sleep(const Duration(milliseconds: 17));
+
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      if (!mounted) return;
+      setState(() {});
+    });
+
+    return const CounterWidget();
   }
 }
