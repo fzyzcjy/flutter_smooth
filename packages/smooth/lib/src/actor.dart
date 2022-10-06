49acbd21f4d254501eb4baad7a92f94735ca0881
fzyzcjy <ch271828n@outlook.com>
2022-10-06 11:41:24 +0800

    fix: in AfterFlushLayout, should call dispatchExtraPointerEvents #6033

--- packages/smooth/lib/src/actor.dart
+++ packages/smooth/lib/src/actor.dart
@@ -2,8 +2,8 @@ import 'dart:developer';
 import 'dart:ui';
 
 import 'package:clock/clock.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth/src/event_dispatcher.dart';
 import 'package:smooth/src/service_locator.dart';
 
 class Actor {
@@ -48,7 +48,7 @@ class Actor {
       //     'shouldShiftOneFrameForInterestVsyncTarget=$shouldShiftOneFrameForInterestVsyncTarget '
       //     'set-interestVsyncTargetTimeByLastPreemptRender=$interestVsyncTargetTimeByLastPreemptRender');
 
-      _hackDispatchExtraPointerEvents();
+      EventDispatcher.dispatchExtraPointerEvents();
 
       // ref: https://github.com/fzyzcjy/yplusplus/issues/5780#issuecomment-1254562485
       // ref: RenderView.compositeFrame
@@ -92,49 +92,5 @@ class Actor {
     }
   }
 
-  // static var _nextDummyPosition = 0.0;
-
-  // TODO just prototype, not final code
-  // #5867
-  void _hackDispatchExtraPointerEvents() {
-    final gestureBinding = GestureBinding.instance;
-
-    // print('hackDispatchExtraPointerEvents '
-    //     'pointer=$pointer '
-    //     'hitTest=${gestureBinding.hitTests[pointer]!}');
-
-    final pendingEvents = gestureBinding.readEnginePendingEventsAndClear();
-    // print(
-    //     'pendingPacket.len=${pendingPacket.data.length} pendingPacket.data=${pendingPacket.data}');
-
-    // // WARN: this fake event is VERY dummy! many fields are not filled in
-    // // so a real consumer of pointer event may get VERY confused!
-    // final event = PointerMoveEvent(
-    //   pointer: pointer,
-    //   position: Offset(_nextDummyPosition, _nextDummyPosition),
-    // );
-    // _nextDummyPosition = (_nextDummyPosition + 10) % 300;
-
-    final interestPipelineOwners = ServiceLocator
-        .instance.auxiliaryTreeRegistry.trees
-        .map((tree) => tree.pipelineOwner)
-        .toList();
-
-    // NOTE only deal with those events that does *not* require a [hitTest]
-    //      pointer *move* events are such kind.
-    final interestEvents = pendingEvents.whereType<PointerMoveEvent>().toList();
-
-    // https://github.com/fzyzcjy/yplusplus/issues/5867#issuecomment-1263053441
-    for (final event in interestEvents) {
-      // TODO this is WRONG, will cause duplicate event sending! #5875
-      gestureBinding.handlePointerEvent(
-        event,
-        filter: (entry) {
-          final target = entry.target;
-          return target is RenderObject &&
-              interestPipelineOwners.contains(target.owner);
-        },
-      );
-    }
-  }
+// static var _nextDummyPosition = 0.0;
 }
