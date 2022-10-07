1712559a2e567171be0d28639a1d4d1af2e2ff84
fzyzcjy <ch271828n@outlook.com>
2022-10-07 10:21:33 +0800

    add ProxyBuildOwner #6042

--- packages/smooth/lib/src/proxy.dart
+++ packages/smooth/lib/src/proxy.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 class ProxyPipelineOwner implements PipelineOwner {
   final PipelineOwner inner;
@@ -55,4 +56,45 @@ class ProxyPipelineOwner implements PipelineOwner {
 
   @override
   void flushSemantics() => inner.flushSemantics();
+}
+
+class ProxyBuildOwner implements BuildOwner {
+  final BuildOwner inner;
+
+  ProxyBuildOwner(this.inner);
+
+  @override
+  VoidCallback? get onBuildScheduled => inner.onBuildScheduled;
+
+  @override
+  set onBuildScheduled(VoidCallback? value) => inner.onBuildScheduled = value;
+
+  @override
+  FocusManager get focusManager => inner.focusManager;
+
+  @override
+  set focusManager(FocusManager value) => inner.focusManager = value;
+
+  @override
+  void scheduleBuildFor(Element element) => inner.scheduleBuildFor(element);
+
+  @override
+  bool get debugBuilding => inner.debugBuilding;
+
+  @override
+  void lockState(VoidCallback callback) => inner.lockState(callback);
+
+  @override
+  void buildScope(Element context, [VoidCallback? callback]) =>
+      inner.buildScope(context, callback);
+
+  @override
+  int get globalKeyCount => inner.globalKeyCount;
+
+  @override
+  void finalizeTree() => inner.finalizeTree();
+
+  @override
+  void reassemble(Element root, DebugReassembleConfig? reassembleConfig) =>
+      inner.reassemble(root, reassembleConfig);
 }
