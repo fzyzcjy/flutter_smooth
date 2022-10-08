d24968bfe2ecd2c2cca76a398bc41278aff75e5a
fzyzcjy <ch271828n@outlook.com>
2022-10-08 10:27:52 +0800

    implement control group #6074

--- packages/smooth_example_control_group/lib/main.dart
+++ packages/smooth_example_control_group/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,105 +14,176 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Example (Control Group)'),
+        ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 48,
+              child: Row(
+                children: const [
+                  Expanded(
+                    child: Align(
+                      alignment: Alignment.topCenter,
+                      child: _SimpleCounter(name: 'P'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(child: _buildPlain()),
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+
+  Widget _buildPlain() {
+    return ListView.builder(
+      itemCount: 1000,
+      itemBuilder: _buildRow,
+    );
+  }
+
+  Widget _buildRow(BuildContext context, int index) {
+    return ListTile(
+      dense: true,
+      visualDensity: VisualDensity.compact,
+      leading: SizedBox(
+        width: 32,
+        height: 32,
+        child: CircleAvatar(
+          backgroundColor: Colors.primaries[index % Colors.primaries.length],
+          child: Text(
+            'G$index',
+            style: const TextStyle(fontSize: 12),
+          ),
+        ),
+      ),
+      title: Container(
+        // just for easy video checking
+        color: index % 10 == 0
+            ? Colors.green
+            : index % 5 == 0
+                ? Colors.pink
+                : null,
+        child: Text(
+          '$index',
+          overflow: TextOverflow.ellipsis,
+        ),
+      ),
+      subtitle: Stack(
+        children: [
+          Text('a\n' * (3 + Random().nextInt(3))),
+        ],
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _SimpleCounter extends StatefulWidget {
+  final String name;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const _SimpleCounter({required this.name});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<_SimpleCounter> createState() => _SimpleCounterState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SimpleCounterState extends State<_SimpleCounter>
+    with SingleTickerProviderStateMixin {
+  late final _controller =
+      AnimationController(duration: const Duration(seconds: 1), vsync: this);
+  double? _prevAnimationValue;
+  var _buildCount = 0, _animationValueChangeCount = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (_, __) {
+        _buildCount++;
+
+        final currAnimationValue = _controller.value;
+        if (_prevAnimationValue != currAnimationValue) {
+          _animationValueChangeCount++;
+        }
+        _prevAnimationValue = currAnimationValue;
+
+        // #6029
+
+        return Timeline.timeSync(
+            '$_buildCount.$_animationValueChangeCount.${widget.name}.SimpleCounter',
+            () {
+          return Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                widget.name,
+                style: const TextStyle(color: Colors.black, fontSize: 8),
+              ),
+              Text(
+                // ignore: prefer_interpolation_to_compose_strings
+                (_buildCount % 1000).toString().padRight(3) +
+                    '|' +
+                    (_animationValueChangeCount % 1000).toString().padRight(3),
+                style: const TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              CustomPaint(
+                painter: _SimpleCounterPainter(
+                  index: _buildCount,
+                ),
+                child: const SizedBox(
+                  height: 48,
+                  width: 24.0 * _SimpleCounterPainter.N,
+                ),
+              ),
+            ],
+          );
+        });
+      },
     );
   }
+}
+
+class _SimpleCounterPainter extends CustomPainter {
+  final int index;
+
+  _SimpleCounterPainter({required this.index});
+
+  static final _painters = List.generate(
+      N, (i) => Paint()..color = [Colors.red, Colors.green, Colors.blue][i]);
+
+  static const N = 3;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawRect(
+      Rect.fromLTWH(
+        size.width / N * (index % N),
+        0,
+        size.width / N,
+        size.height,
+      ),
+      _painters[index % N],
+    );
+  }
+
+  @override
+  bool shouldRepaint(_SimpleCounterPainter oldDelegate) =>
+      oldDelegate.index != index;
 }
