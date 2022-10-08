c0d698dd526c420e725873e1e32fc8f1f4005964
fzyzcjy <ch271828n@outlook.com>
2022-10-08 09:40:26 +0800

    minor change logging

--- packages/smooth/lib/src/log.dart
+++ packages/smooth/lib/src/log.dart
@@ -1,6 +1,7 @@
-// TODO may not exist after prorotyping
 import 'package:clock/clock.dart';
+import 'package:flutter/foundation.dart';
 
+// TODO may not exist after prorotyping
 class SimpleLog {
   static final instance = SimpleLog._();
 
@@ -8,7 +9,11 @@ class SimpleLog {
 
   final _messages = <String>[];
 
-  void log(String value) => _messages.add('[${clock.now()}] $value');
+  void log(String value) {
+    final message = '[${clock.now()}] $value';
+    _messages.add(message);
+    debugPrint(message);
+  }
 
   void dumpAndReset() {
     for (final log in _messages) {
