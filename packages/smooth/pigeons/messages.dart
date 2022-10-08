7b6f5ca1db52303bbbf89cbd832aba8334dc48e9
fzyzcjy <ch271828n@outlook.com>
2022-10-08 08:10:39 +0800

    add pigeon #6069

---
+++ packages/smooth/pigeons/messages.dart
@@ -0,0 +1,21 @@
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(PigeonOptions(
+  dartOut: 'lib/src/messages.dart',
+  javaOut: 'android/src/main/java/com/cjy/smooth/Messages.java',
+  objcHeaderOut: 'ios/Classes/messages.h',
+  objcSourceOut: 'ios/Classes/messages.m',
+  javaOptions: JavaOptions(
+    package: 'com.cjy.smooth',
+  ),
+  objcOptions: ObjcOptions(
+    prefix: 'CS',
+  ),
+  dartOptions: DartOptions(copyrightHeader: [
+    'ignore_for_file: avoid-non-null-assertion, prefer_constructors_over_static_methods, unnecessary_import'
+  ]),
+))
+@HostApi()
+abstract class SmoothHostApi {
+  int systemUptimeMicroseconds();
+}
