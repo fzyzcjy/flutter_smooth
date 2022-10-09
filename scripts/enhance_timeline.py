8533998448d9fa602a1845a6e93c9bbdcb1dcf92
fzyzcjy <ch271828n@outlook.com>
2022-10-10 07:36:59 +0800

    further impl vsync, not done #6107

--- scripts/enhance_timeline.py
+++ scripts/enhance_timeline.py
@@ -1,8 +1,9 @@
 import json
 import re
 from argparse import ArgumentParser
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Callable, Dict, List, Tuple
+from typing import Callable, Dict, List
 from zipfile import ZipFile
 
 parser = ArgumentParser()
@@ -49,11 +50,38 @@ def synthesize_long_event_matching_filter(filter_event: Callable[[str], bool], s
     data['traceEvents'] += new_events
 
 
-VsyncRange = Tuple[int, int]
+@dataclass
+class VsyncRange:
+    start: int
+    end: int
+
+    @property
+    def duration(self):
+        return self.end - self.start
 
 
 def parse_vsync_info() -> List[VsyncRange]:
-    return TODO
+    raw_positions: List[int] = []
+    for e in data['traceEvents']:
+        if e['name'] == 'VSYNC' and e['ph'] == 'B':
+            raw_positions.append(e['ts'])
+    sorted_raw_positions = sorted(raw_positions)
+
+    ranges: List[VsyncRange] = []
+    prev_vsync_end_time = 9999999999999
+    for raw_position in sorted_raw_positions:
+        duration = round(1 / 60 * 1000000)
+        raw_end = raw_position + duration
+        ranges += reversed([
+            VsyncRange(
+                start=start,
+                # -1us to avoid multi events be treated as one (?)
+                end=start + duration - 1
+            )
+            for start in range(raw_position, prev_vsync_end_time - 10, -duration)
+        ])
+        prev_vsync_end_time = raw_end
+    return ranges
 
 
 def modify_vsync_events(vsync_ranges: List[VsyncRange]):
@@ -65,11 +93,11 @@ def modify_vsync_events(vsync_ranges: List[VsyncRange]):
     # add events
     data['traceEvents'] += [
         e
-        for start, end in vsync_ranges
+        for vsync_range in vsync_ranges
         for e in synthesize_event(
             name='VSYNC',
-            start_us=start,
-            duration_us=end - start,
+            start_us=vsync_range.start,
+            duration_us=vsync_range.duration,
             tid=-1000000,
         )
     ]
