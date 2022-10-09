d05b1a75faef04e4ed18e56a643430c6d8c235b3
fzyzcjy <ch271828n@outlook.com>
2022-10-09 14:57:47 +0800

    experiment #6099

--- packages/smooth_example_control_group/lib/main.dart
+++ packages/smooth_example_control_group/lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:smooth_example_control_group/experiment_list_view.dart';
-import 'package:smooth_example_control_group/experiment_rasterizer/experiment_rasterizer.dart';
 
 void main() {
   // experimentRasterizerStandard();
   // experimentRasterizerTwoRenderZeroRender();
   // experimentRasterizerAnotherTwoRenderZeroRender();
   // experimentRasterizerTwoRenderZeroRenderThirdExample();
-  experimentRasterizerTwoRenderZeroRenderFourthExample();
-  // runApp(const MyApp());
+  // experimentRasterizerTwoRenderZeroRenderFourthExample();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -21,17 +21,60 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Example (Control Group)'),
         ),
-        body: ListView(
-          children: [
-            ListTile(
-              title: const Text('ListView'),
-              onTap: () => Navigator.push<dynamic>(
-                  context,
-                  MaterialPageRoute<dynamic>(
-                      builder: (_) => const ExperimentListView())),
-            ),
-          ],
+        body: Builder(
+          builder: (context) => ListView(
+            children: [
+              ListTile(
+                title: const Text('ListView'),
+                onTap: () => Navigator.push<dynamic>(
+                    context,
+                    MaterialPageRoute<dynamic>(
+                        builder: (_) => const ExperimentListView())),
+              ),
+              ListTile(
+                title: const Text('ListView'),
+                onTap: () => Navigator.push<dynamic>(
+                    context,
+                    MaterialPageRoute<dynamic>(
+                        builder: (_) => const ExperimentFullScreenColor())),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class ExperimentFullScreenColor extends StatefulWidget {
+  const ExperimentFullScreenColor({Key? key}) : super(key: key);
+
+  @override
+  State<ExperimentFullScreenColor> createState() =>
+      _ExperimentFullScreenColorState();
+}
+
+class _ExperimentFullScreenColorState extends State<ExperimentFullScreenColor> {
+  var count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    count++;
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      if (!mounted) return;
+      setState(() {});
+    });
+
+    const colors = [
+      Colors.red,
+      Colors.green,
+      Colors.blue,
+      Colors.purple,
+    ];
+
+    return Scaffold(
+      body: Container(
+        color: colors[count % colors.length],
       ),
     );
   }
