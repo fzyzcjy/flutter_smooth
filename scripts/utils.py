75a07eb820950303ab9d90d117efd12d253c152d
fzyzcjy <ch271828n@outlook.com>
2022-10-09 20:32:51 +0800

    minor extract #6105

---
+++ scripts/utils.py
@@ -0,0 +1,6 @@
+import os
+
+
+def run_command(command):
+    print(f'>>> {command}')
+    os.system(command)
--- scripts/video_to_frame.py
+++ scripts/video_to_frame.py
@@ -1,12 +1,7 @@
-import os
 from argparse import ArgumentParser
 from pathlib import Path
 
-
-def _run_command(command):
-    print(f'>>> {command}')
-    os.system(command)
-
+from utils import run_command
 
 parser = ArgumentParser()
 parser.add_argument('input')
@@ -18,9 +13,9 @@ dir_output = Path(args.output)
 
 assert path_input.exists()
 
-_run_command(f'mkdir -p {dir_output}')
-_run_command(f'rm {dir_output}/*.jpg')
-_run_command(
+run_command(f'mkdir -p {dir_output}')
+run_command(f'rm {dir_output}/*.jpg')
+run_command(
     f"ffmpeg -i {path_input} -vsync 0 -frame_pts true "
     # add text about time information at left-top
     "-vf drawtext=fontfile=/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf:fontsize=80:text='%{pts}':fontcolor=white@0.8:x=7:y=7 "
