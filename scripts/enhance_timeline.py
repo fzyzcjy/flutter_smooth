21341bab0ff1002e430847dea108cdc5634e6545
fzyzcjy <ch271828n@outlook.com>
2022-10-10 08:22:37 +0800

    synthesize_zero_raster_in_interval_events, synthesize_multi_raster_in_interval_events #6102

--- scripts/enhance_timeline.py
+++ scripts/enhance_timeline.py
@@ -37,15 +37,46 @@ def synthesize_long_event_matching_filter(data, filter_event: Callable[[str], bo
                 start_us=e['ts'],
                 tid=synthesize_tid,
             )
-    data['traceEvents'] += new_events
+    return new_events
 
 
 def parse_vsync_positions(data) -> List[int]:
-    vsync_positions = []
-    for e in data['traceEvents']:
-        if e['ph'] == 'B' and e['name'] == 'VSYNC':
-            vsync_positions.append(e['ts'])
-    return sorted(vsync_positions)
+    return sorted([
+        e['ts'] for e in data['traceEvents']
+        if e['ph'] == 'B' and e['name'] == 'VSYNC'
+    ])
+
+
+def parse_raster_end_positions(data) -> List[int]:
+    return sorted([
+        e['ts'] for e in data['traceEvents']
+        if e['ph'] == 'E' and e['name'] == 'GPURasterizer::Draw'
+    ])
+
+
+def synthesize_abnormal_raster_in_interval_events(vsync_positions: List[int], raster_end_positions: List[int]):
+    new_events = []
+    raster_index = 0
+    for vsync_index in range(len(vsync_positions) - 1):
+        num_raster_in_vsync_interval = 0
+        while raster_index < len(raster_end_positions) and \
+                raster_end_positions[raster_index] < vsync_positions[vsync_index + 1]:
+            raster_index += 1
+            num_raster_in_vsync_interval += 1
+
+        if raster_index >= len(raster_end_positions):
+            break
+
+        event_common_args = dict(
+            start_us=vsync_positions[vsync_index],
+            duration_us=vsync_positions[vsync_index + 1],
+            tid=-999999,
+        )
+        if num_raster_in_vsync_interval == 0:
+            new_events += synthesize_event(name='Jank:ZeroRasterEndInVsyncInterval', **event_common_args)
+        elif num_raster_in_vsync_interval >= 2:
+            new_events += synthesize_event(name='Waste:MultiRasterEndInVsyncInterval', **event_common_args)
+    return new_events
 
 
 def main():
@@ -59,12 +90,12 @@ def main():
     data = json.loads(path_input.read_text())
 
     vsync_positions = parse_vsync_positions(data)
+    raster_end_positions = parse_raster_end_positions(data)
 
-    synthesize_long_event_matching_filter(
-        data,
-        lambda s: re.match(r'.*\.S\.SimpleCounter', s) is not None,
-        synthesize_tid=-999999,
-    )
+    data['traceEvents'] += synthesize_abnormal_raster_in_interval_events(vsync_positions, raster_end_positions)
+
+    data['traceEvents'] += synthesize_long_event_matching_filter(
+        data, lambda s: re.match(r'.*\.S\.SimpleCounter', s) is not None, synthesize_tid=-999998)
 
     print(f'#events={len(data["traceEvents"])}')
     path_output.write_text(json.dumps(data))
