3042f3cb09a216012df9884cf74d05b5e85122cc
fzyzcjy <ch271828n@outlook.com>
2022-10-12 21:44:45 +0800

    minor

--- scripts/timeline/visualize_scroll.py
+++ scripts/timeline/visualize_scroll.py
@@ -25,7 +25,7 @@ if 0:
     args = parser.parse_args()
     path_input = args.input
 else:
-    path_input = '/Users/tom/Downloads/dart_devtools_2022-10-12_21_06_30.980.json'
+    path_input = '/Users/tom/Downloads/dart_devtools_2022-10-12_21_42_24.465.json'
 
 # %%
 
