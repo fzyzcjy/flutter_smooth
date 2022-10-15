666fd8221e58090838657f2f04686e44875e19b7
fzyzcjy <ch271828n@outlook.com>
2022-10-15 11:51:31 +0800

    add UT for brake controller #6210

---
+++ packages/smooth/test/src/brake/brake_controller_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:smooth/src/brake/brake_controller.dart';
+
+void main() {
+  group('BrakeController', () {
+    testWidgets('simple', (tester) async {
+      final controller = BrakeController();
+
+      await tester.pumpWidget(const CircularProgressIndicator());
+
+      expect(controller.brakeModeActive, false);
+
+      controller.activateBrakeMode();
+      expect(controller.brakeModeActive, true);
+
+      controller.activateBrakeMode();
+      expect(controller.brakeModeActive, true);
+
+      await tester.pump();
+
+      expect(controller.brakeModeActive, false,
+          reason: 'auto become false after a frame');
+
+      controller.activateBrakeMode();
+      expect(controller.brakeModeActive, true);
+    });
+  });
+}
