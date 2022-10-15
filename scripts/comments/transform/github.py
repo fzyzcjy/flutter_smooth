b73b78c6fa2ce60626682ceac017b34442253b11
fzyzcjy <ch271828n@outlook.com>
2022-10-15 21:32:19 +0800

    display root post close #103

--- scripts/comments/transform/github.py
+++ scripts/comments/transform/github.py
@@ -2,12 +2,18 @@ from comments.common import TransformedComment
 
 
 def _transform_comment(comment_raw, retrieve_time, link):
-    if comment_raw['isMinimized']:
+    if comment_raw.get('isMinimized', False):
         return None
 
+    body = comment_raw['body']
+
+    title = comment_raw.get('title')
+    if title is not None:
+        body = f'### {title}\n\n{body}'
+   
     return TransformedComment(
         author=comment_raw['author']['login'],
-        body=comment_raw['body'],
+        body=body,
         link=link,  # do not have per-comment link yet
         create_time=comment_raw['createdAt'],
         retrieve_time=retrieve_time,
@@ -18,6 +24,8 @@ def transform(data_raw):
     metadata = data_raw['metadata']
     retrieve_time = metadata['retrieve_time']
     link = f'https://github.com/{metadata["org"]}/{metadata["repo"]}/issues/{metadata["issue"]}'
+
+    yield _transform_comment(data_raw['content'], retrieve_time, link)
 
     for comment_raw in data_raw['content']['comments']:
         transformed_comment = _transform_comment(comment_raw, retrieve_time, link)
