f02cbbf712df84573395a631472c9fc21a374d82
fzyzcjy <ch271828n@outlook.com>
2022-10-15 21:18:43 +0800

    improve DiscussionComment

--- website/src/components/DiscussionComment.tsx
+++ website/src/components/DiscussionComment.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 // noinspection JSUnusedGlobalSymbols
 export default function DiscussionComment({children, author, link, createTime, retrieveTime}) {
-    // TODO
-    return <div className="bg-blue-500">
-        <span>author={author}</span>
-        <span>link={link}</span>
-        <span>createTime={createTime}</span>
-        <span>retrieveTime={retrieveTime}</span>
+    return <div className="discussion-comment border rounded mb-8 px-4 py-4">
+        <div className="flex flex-row mb-4">
+            <span className="font-semibold">{author}</span>
+            &emsp;
+            <span>{createTime}</span>
+            <span className="flex-1"/>
+            <a href={link}>Original link</a>
+        </div>
         {children}
     </div>
 }
