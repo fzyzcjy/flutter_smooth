cfd438618f6c4e4673171b67f1faced1b7703f31
fzyzcjy <ch271828n@outlook.com>
2022-10-15 21:49:51 +0800

    add icon to source #104

--- website/src/components/DiscussionComment.tsx
+++ website/src/components/DiscussionComment.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import {FaGithub} from 'react-icons/fa';
 
 const sourceTitleMap = {
     'github': 'GitHub',
 }
+const sourceIconMap = {
+    // TODO
+    'github': <FaGithub className="inline-block pb-1"/>,
+}
 
 // noinspection JSUnusedGlobalSymbols
 export default function DiscussionComment({children, author, link, createTime, source, retrieveTime}) {
-    const sourceTitle = sourceTitleMap[source]
     return <div className="discussion-comment border rounded mb-8 px-4 py-4">
         <div className="flex flex-row mb-4">
             <span className="font-semibold">{author}</span>
             &emsp;
             <span>{createTime}</span>
             <span className="flex-1"/>
-            <a href={link}>{sourceTitle}</a>
+            <a href={link}>
+                {sourceIconMap[source]}
+                &nbsp;
+                {sourceTitleMap[source]}
+            </a>
         </div>
         {children}
     </div>
