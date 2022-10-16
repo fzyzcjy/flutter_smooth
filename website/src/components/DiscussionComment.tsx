f7a86e27b5cc7c3af2fc51ee75c312e046df58ca
fzyzcjy <ch271828n@outlook.com>
2022-10-16 10:05:15 +0800

    minor change ui

--- website/src/components/DiscussionComment.tsx
+++ website/src/components/DiscussionComment.tsx
@@ -6,12 +6,12 @@ const sourceTitleMap = {
 }
 const sourceIconMap = {
     // TODO
-    'github': <FaGithub className="inline-block pb-1"/>,
+    'github': <FaGithub className="inline-block pt-1"/>,
 }
 
 // noinspection JSUnusedGlobalSymbols
 export default function DiscussionComment({children, author, link, createTime, source, retrieveTime}) {
-    return <div className="discussion-comment border rounded mb-8 px-4 py-4">
+    return <div className="discussion-comment border border-solid border-slate-200 rounded mb-8 px-4 py-4">
         <div className="flex flex-row mb-4">
             <span className="font-semibold">{author}</span>
             &emsp;
