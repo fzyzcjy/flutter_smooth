98f2e6abdf93f388d8e548bd947887862fde8f6f
fzyzcjy <ch271828n@outlook.com>
2022-10-16 09:45:38 +0800

    minor

--- scripts/readme_video.py
+++ scripts/readme_video.py
@@ -29,11 +29,11 @@ class Bbox:
 plain_bbox = Bbox.from_p1_p2((200, 0), (1100, 1050))
 smooth_bbox = Bbox.from_p1_p2((100, 0), (1000, 1050))
 
-p_output = dir_video / '2_output.mp4'
+p_output = dir_video / 'output.mp4'
 p_output.unlink(missing_ok=True)
 
-in_plain = ffmpeg.input(str(dir_video / '1_plain_raw.mp4'))
-in_smooth = ffmpeg.input(str(dir_video / '1_smooth_raw.mp4'))
+in_plain = ffmpeg.input(str(dir_video / 'list_view/raw_plain.mp4'))
+in_smooth = ffmpeg.input(str(dir_video / 'list_view/raw_smooth.mp4'))
 
 cropped_plain = ffmpeg.crop(in_plain, *plain_bbox.xywh)
 cropped_smooth = ffmpeg.crop(in_smooth, *smooth_bbox.xywh)
