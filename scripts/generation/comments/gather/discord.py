e3be13417e19e2812239ea3e67608cbf30b5ed3d
fzyzcjy <ch271828n@outlook.com>
2022-10-17 10:37:37 +0800

    impl discord exporter

--- scripts/generation/comments/gather/discord.py
+++ scripts/generation/comments/gather/discord.py
@@ -1,30 +1,74 @@
+import os
+import re
+from pathlib import Path
+
+from utils import run_command
+
+_dir_out = Path('/tmp/out')
+
+
+def _discord_token():
+    return os.environ['DISCORD_TOKEN']
+
+
+def _run_discord_chat_exporter(command, *args):
+    run_command(
+        f'docker run -it -v {_dir_out}:/out '
+        f'tyrrrz/discordchatexporter:stable '
+        f'{command} '
+        f'--token {_discord_token()} '
+        f'{" ".join(args)}'
+    )
+
+
+def _run_export_command(channel: str, *args):
+    _run_discord_chat_exporter(
+        'export',
+        '--format', 'Json',
+        '--channel', channel,
+        *args,
+    )
+
+
 def gather_range(start_url: str, end_url_inclusive: str):
-    TODO
+    matcher_start = re.match(r'https://discord.com/channels/(\d+)/(\d+)/(\d+)', start_url)
+    matcher_end = re.match(r'https://discord.com/channels/(\d+)/(\d+)/(\d+)', end_url_inclusive)
+
+    channel_id = matcher_start.group(1)
+    assert channel_id == matcher_end.group(1)
+
+    after = matcher_start.group(3)
+    before = matcher_end.group(3)
+
+    _run_export_command('export', channel_id, f'--after {after} --before {before}')
 
 
 def gather_all_in_channel(url: str):
-    TODO
+    channel_id = re.match(r'https://discord.com/channels/(\d+)/(\d+)', url).group(1)
+    _run_export_command('export', channel_id)
 
 
 def main():
     # hackers-framework
-    gather_range(
-        start_url='https://discord.com/channels/608014603317936148/608021234516754444/1021783497112821861',
-        end_url_inclusive='https://discord.com/channels/608014603317936148/608021234516754444/1021807402510716970',
-    )
-    gather_range(
-        start_url='https://discord.com/channels/608014603317936148/608021234516754444/1021917668288245772',
-        end_url_inclusive='https://discord.com/channels/608014603317936148/608021234516754444/1024852804029927537',
-    )
+    if 0:
+        gather_range(
+            start_url='https://discord.com/channels/608014603317936148/608021234516754444/1021783497112821861',
+            end_url_inclusive='https://discord.com/channels/608014603317936148/608021234516754444/1021807402510716970',
+        )
+        gather_range(
+            start_url='https://discord.com/channels/608014603317936148/608021234516754444/1021917668288245772',
+            end_url_inclusive='https://discord.com/channels/608014603317936148/608021234516754444/1024852804029927537',
+        )
     gather_range(
         start_url='https://discord.com/channels/608014603317936148/608021234516754444/1029371064066785340',
         end_url_inclusive='https://discord.com/channels/608014603317936148/608021234516754444/1029757386598121502',
     )
 
     # the discord "thread"
-    gather_all_in_channel(
-        url='https://discord.com/channels/608014603317936148/1021987751710699632',
-    )
+    if 0:
+        gather_all_in_channel(
+            url='https://discord.com/channels/608014603317936148/1021987751710699632',
+        )
 
 
 if __name__ == '__main__':
