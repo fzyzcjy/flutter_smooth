d75b9f6229fd2776d6c3b8183183f3846ff3db20
fzyzcjy <ch271828n@outlook.com>
2022-10-17 14:54:24 +0800

    add ci mode #134

--- scripts/generation/__main__.py
+++ scripts/generation/__main__.py
@@ -1,14 +1,23 @@
+from argparse import ArgumentParser
+
 from generation import fetch_github_pr, generate_pr_status
 from .comments.gather import __main__ as gather_main
 from .comments.transform import __main__ as transform_main
 
 
-def main():
+def main(ci: bool):
+    print(f'main start (ci={ci})')
+   
     fetch_github_pr.main()
     generate_pr_status.main()
-    gather_main.main()
+    if not ci:
+        gather_main.main()
     transform_main.main()
 
 
 if __name__ == '__main__':
-    main()
+    parser = ArgumentParser()
+    parser.add_argument('--ci', action='store_true')
+    args = parser.parse_args()
+
+    main(ci=args.ci)
