c9a0c99fbb5b5a0b71d2b897c50cac504b82964a
fzyzcjy <ch271828n@outlook.com>
2022-10-21 20:49:49 +0800

    Let CI "generation" generate more things, mostly auto fetch new github comments #152

--- scripts/generation/comments/gather/__main__.py
+++ scripts/generation/comments/gather/__main__.py
@@ -1,10 +1,11 @@
 from . import github, discord, google_doc_comments
 
 
-def main():
+def main(ci: bool = False):
     github.main()
-    discord.main()
-    google_doc_comments.main()
+    if not ci:
+        discord.main()
+        google_doc_comments.main()
 
 
 if __name__ == '__main__':
